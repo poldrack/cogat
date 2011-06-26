9d89d09119802a0d5f4e93d0d671803ea12c7c9a
Russ Poldrack <poldrack@gmail.com>
2011-06-26 15:23:02 -0500

    fixed descended_from problem, added relation range and domain.

--- create_owl.py
+++ create_owl.py
@@ -19,14 +19,14 @@ owl_classes[-1]=owl_classes[-1].replace('</rdf:RDF>','')
 
 # 3. add additional namespaces
 
-additional_namespaces=['xmlns:OBO_REL="http://purl.org/obo/owl/OBO_REL#"','xmlns:snap="http://www.ifomis.org/bfo/1.1/snap#"','xmlns:obo="http://purl.obolibrary.org/obo/"','xmlns:ro="http://www.obofoundry.org/ro/ro.owl#"','xmlns:owl="http://www.w3.org/2002/07/owl#"','xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"','xmlns:span="http://www.ifomis.org/bfo/1.1/span#"']
+additional_namespaces=['xmlns:OBO_REL="http://purl.org/obo/owl/OBO_REL#"','xmlns:snap="http://www.ifomis.org/bfo/1.1/snap#"','xmlns:obo="http://purl.obolibrary.org/obo/"','xmlns:ro="http://www.obofoundry.org/ro/ro.owl#"','xmlns:owl="http://www.w3.org/2002/07/owl#"','xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"','xmlns:span="http://www.ifomis.org/bfo/1.1/span#"','xmlns:cogpo="http://www.cogpo.org/ontologies/working/CogPOver2011.owl#"']
 
 for a in additional_namespaces:
     rdf_preamble=rdf_preamble.replace('> ','\n\t%s> '%a)
 
 print rdf_preamble
 
-rdf_entities=['dc "http://purl.org/dc/elements/1.1/"','ro "http://www.obofoundry.org/ro/ro.owl#"','cogat "http://www.cognitiveatlas.org/ontology/cogat.owl#"','rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"','skos "http://www.w3.org/2004/02/skos/core#"','rdfs "http://www.w3.org/2000/01/rdf-schema#"','span "http://www.ifomis.org/bfo/1.1/span#"']
+rdf_entities=['dc "http://purl.org/dc/elements/1.1/"','ro "http://www.obofoundry.org/ro/ro.owl#"','cogat "http://www.cognitiveatlas.org/ontology/cogat.owl#"','rdf "http://www.w3.org/1999/02/22-rdf-syntax-ns#"','skos "http://www.w3.org/2004/02/skos/core#"','rdfs "http://www.w3.org/2000/01/rdf-schema#"','span "http://www.ifomis.org/bfo/1.1/span#"','cogpo "http://www.cogpo.org/ontologies/working/CogPOver2011.owl#"']
 
 entities='<!DOCTYPE rdf:RDF [\n'
 for a in rdf_entities:
@@ -49,7 +49,9 @@ for a in annotation_properties:
 # add object properties#
 properties=properties+'\n\n<!--\n//Object Properties\n-->\n\n'
 
-object_properties=['<owl:ObjectProperty rdf:about="&ro;has_part"/>','<owl:ObjectProperty rdf:about="&cogat;measured_by"/>','<owl:ObjectProperty rdf:about="&cogat;descended_from"/>']
+object_properties=['<owl:ObjectProperty rdf:about="&ro;has_part"/>','<owl:ObjectProperty rdf:about="&cogat;measured_by"><rdfs:label>measured_by</rdfs:label><rdfs:domain rdf:resource="&cogat;MentalConcept"/><rdfs:range rdf:resource="&cogpo;COGPO_00049"/></owl:ObjectProperty>','<owl:ObjectProperty rdf:about="&cogat;descended_from"><rdfs:label>descended_from</rdfs:label><rdfs:domain rdf:resource="&cogpo;COGPO_00049"/><rdfs:range rdf:resource="&cogpo;COGPO_00049"/></owl:ObjectProperty>']
+
+# NOTE: need to change the measured_by relation to have contrasts rather than tasks as its domain
 
 for a in object_properties:
     properties=properties+'%s\n'%a
@@ -57,6 +59,7 @@ for a in object_properties:
 properties=properties+'\n\n'
 
 properties=properties+'<owl:Class rdf:about="&cogat;MentalConcept">\n\t<rdfs:subClassOf rdf:resource="&skos;Concept"/>\n</owl:Class>\n\n'
+
 
 # 5. fix each class
 owl_classes_fixed=[]
@@ -125,7 +128,8 @@ for c in owl_classes:
                 owl_task_dict[id][f]=getcontent(e)
     owl_task_dict[id]['dc:Title']=owl_task_dict[id]['dc:Title'].replace('Cognitive Atlas : Lexicon : ','')
     owl_task_dict[id]['relations']=[]
-    owl_task_dict[id]['superClass']='&span;Process'
+    #owl_task_dict[id]['superClass']='&span;Process'
+    owl_task_dict[id]['superClass']='&cogpo;COGPO_00049'
     ctr+=1
 
 # read in relations
@@ -172,6 +176,7 @@ attrs_to_loop=['dc:Title','dc:Contributor','dc:Date','skos:definition','skos:pre
 for a in owl_dict.iterkeys():
     d=owl_dict[a]
     f.write('<owl:Class rdf:about="&cogat;%s">\n'%a)
+    f.write('\t<rdfs:label>%s</rdfs:label>\n'%d['dc:Title'])
     f.write('\t<dc:identifier>%s</dc:identifier>\n'%a)
     for l in attrs_to_loop:
         f.write('\t<%s>%s</%s>\n'%(l,d[l],l))
@@ -195,7 +200,7 @@ for a in owl_dict.iterkeys():
         for m in set(measured_by_relations):
             f.write('\t<rdfs:subClassOf>\n\t\t<owl:Restriction>\n\t\t\t<owl:onProperty rdf:resource="&cogat;measured_by"/>\n\t\t\t<owl:someValuesFrom rdf:resource="&cogat;%s"/>\n\t\t</owl:Restriction>\n\t</rdfs:subClassOf>\n'%m)
         f.write('\t<rdfs:subClassOf>\n\t\t<owl:Restriction>\n\t\t\t<owl:onProperty rdf:resource="&cogat;measured_by"/>\n\t\t\t\t<owl:allValuesFrom>\n\t\t\t\t\t<owl:Class>\n\t\t\t\t\t\t<owl:unionOf rdf:parseType="Collection">\n')
-        print measured_by_relations
+        #print measured_by_relations
         for m in set(measured_by_relations):
             f.write('\t\t\t\t\t\t<rdf:Description rdf:about="&cogat;%s"/>\n'%m)
         f.write('\t\t\t\t\t\t</owl:unionOf>\n\t\t\t\t\t</owl:Class>\n\t\t\t\t</owl:allValuesFrom>\n\t\t</owl:Restriction>\n\t</rdfs:subClassOf>\n\n')
@@ -208,10 +213,14 @@ for a in owl_task_dict.iterkeys():
     d=owl_task_dict[a]
     f.write('<owl:Class rdf:about="&cogat;%s">\n'%a)
     f.write('\t<dc:identifier>%s</dc:identifier>\n'%a)
+    f.write('\t<rdfs:label>%s</rdfs:label>\n'%d['dc:Title'])
     for l in attrs_to_loop:
         f.write('\t<%s>%s</%s>\n'%(l,d[l],l))
-        if r[0]=='T15' and owl_task_dict.has_key(r[1]):
-                f.write('\t<rdfs:subClassOf>\n\t\t<owl:Restriction>\n\t\t\t<owl:onProperty rdf:resource="&cogat;measured_by"/>\n\t\t\t<owl:someValuesFrom rdf:resource="&cogat;%s"/>\n\t\t</owl:Restriction>\n\t</rdfs:subClassOf>\n'%r[1])
+    if len(d['relations'])>0:
+        for r in d['relations']:
+            if r[0]=='T15' and owl_task_dict.has_key(r[1]):
+                print r
+                f.write('\t<rdfs:subClassOf>\n\t\t<owl:Restriction>\n\t\t\t<owl:onProperty rdf:resource="&cogat;descended_from"/>\n\t\t\t<owl:someValuesFrom rdf:resource="&cogat;%s"/>\n\t\t</owl:Restriction>\n\t</rdfs:subClassOf>\n'%r[1])
     f.write('\t<rdfs:subClassOf rdf:resource="%s"/>\n'%d['superClass'])
     f.write('</owl:Class>\n\n\n')
                 
